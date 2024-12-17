99b53631ab7572551548330db11d17ca1a57a46e
1ASPER <asperthex@gmail.com>
2024-12-17 08:52:04 +0600

    parsing data (fetching)

--- src/App.js
+++ src/App.js
@@ -4,105 +4,34 @@ import { Typography } from './components/Typography';
 import { Heading } from './components/Heading';  
 import { Tooltip } from './components/Tooltip';
 import { Accordion } from './components/Accordion';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
 
 
 function App() {
-  return (
-    <div className="wrapper">
 
-        {/* Button */}
-      
-        <div className="button-container">
-            
-            <Button className='button' variant ='primary' size='large'>lg, primary</Button>
-            <Button className='button' variant ='primary' size='medium'>md, primary</Button>
-            <Button className='button' variant ='primary' size='small'>sm, primary</Button>
+    const [cryptoData, setCryptoData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [search, setSearch] = useState('');
 
-            <Button className='button' variant ='bordered' size='large'>lg, bordered</Button>
-            <Button className='button' variant ='bordered' size='medium'>md, bordered</Button>
-            <Button className='button' variant ='bordered' size='small'>sm, bordered</Button>
-            
+    const fetchData = async () => {
+        console.log('Fetching data...'); // This will be displayed first (log for check)
+        const response = await axios.get('https://api.coinlore.net/api/tickers/');
+        console.log('Data fetched:', response.data.data); // This is the data we want to display (log for data)
+
+        setCryptoData(response.data.data);
+        setFilteredData(response.data.data);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>
         </div>
-
-        {/* Input */}
-
-        <div className="input-container">
-            <Input type="text" placeholder="Ivan Ivanov" label="Name" />
-            <Input type="text" placeholder="Focus" label="Name" />
-            <Input type="text" placeholder="Danger" danger label="Name" />
-            {/* <Input type="text" placeholder="Enter your password" disabled label="Name" value="Disabled"/> */}
-        </div>
-
-        {/* Typography */}
-
-        <div className="typography-container">
-            <Typography textSize="sm">
-                SM. Our mission is to redefine the model of learning and re-engineer
-                its process by providing innovative administrative and instructional
-                technologies. Click here for our beliefs. If you are a school
-                administrator and would like to use ecourse.org for your classes,
-                please create school profiles. Instructors and students may signup
-                independent user account. For questions, contact us.
-            </Typography>
-            <Typography textSize="md">
-                MD. Our mission is to redefine the model of learning and re-engineer
-                its process by providing innovative administrative and instructional
-                technologies. Click here for our beliefs. If you are a school
-                administrator and would like to use ecourse.org for your classes,
-                please create school profiles. Instructors and students may signup
-                independent user account. For questions, contact us.
-            </Typography>
-            <Typography textSize="lg">
-                LG. Our mission is to redefine the model of learning and re-engineer
-                its process by providing innovative administrative and instructional
-                technologies. Click here for our beliefs. If you are a school
-                administrator and would like to use ecourse.org for your classes,
-                please create school profiles. Instructors and students may signup
-                independent user account. For questions, contact us.
-            </Typography>
-        </div>
-
-        {/* Heading */}
-
-        <div className="heading-container">
-            <Heading level={1} className="heading">H1. Heading 1</Heading>
-            <Heading level={2} className="heading">H2. Heading 2</Heading>
-            <Heading level={3} className="heading">H3. Heading 3</Heading>
-            <Heading level={4} className="heading">H4. Heading 4</Heading>
-            <Heading level={5} className="heading">H5. Heading 5</Heading>
-            <Heading level={6} className="heading">H6. Heading 6</Heading>
-        </div>
-
-        {/* Tooltip */}
-
-        <div className="tooltip-container">
-            <Tooltip text="I am a tooltip">Tooltip Top</Tooltip>
-            <Tooltip text="I am a tooltip" position="left">Tooltip Left</Tooltip>
-            <Tooltip text="I am a tooltip" position="right">Tooltip Right</Tooltip>
-            <Tooltip text="I am a tooltip" position="bottom">Tooltip Bottom</Tooltip>
-        </div>
-
-        {/* Accordion */}
-
-        <div className="accordion-container">
-            <Accordion title="Accordion 1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat.
-            </Accordion>
-            <Accordion title="Accordion 2" defaultOpen>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat.
-            </Accordion>
-        </div>
-
-
-      
-    </div>
-  );
+    );
 }
 
 export default App;
