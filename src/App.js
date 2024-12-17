3dda6822bd28af01f593423c13a58f479de69c51
1ASPER <asperthex@gmail.com>
2024-12-17 09:35:43 +0600

    finished (i hope)

--- src/App.js
+++ src/App.js
@@ -28,10 +28,50 @@ function App() {
         fetchData();
     }, []);
 
-    return (
-        <div>
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+        setFilteredData(
+          cryptoData.filter((crypto) =>
+            `${crypto.name} ${crypto.symbol}`
+              .toLowerCase()
+              .includes(e.target.value.toLowerCase())
+          )
+        );
+      };
+    
+      return (
+        <div style={{ padding: "20px", display: "flex", flexDirection: "column", gap: "20px" }}>
+          <div className="flex-column-start">
+            <Heading variant="h1">Cryptocurrency Prices</Heading>
+            <Button onClick={fetchData} variant = "bordered">Update</Button>
+            <Input
+              value={search}
+              onChange={handleSearch}
+              placeholder="Search"
+            />
+          </div>
+          <div>
+          {filteredData.map((crypto) => (
+            <Accordion title={crypto.name}>
+              <div>
+                  <div><b>Symbol:</b> {crypto.symbol}</div>
+                  <div><b>Price USD:</b> {crypto.price_usd}</div>
+                  <div><b>Price BTC:</b> {crypto.price_btc}</div>
+                  <Tooltip text="The market capitalization of a cryptocurrency is calculated by multiplying the number of coins in circulation by the current price">
+                    <span><b>Market Cap USD:</b></span>
+                  </Tooltip>
+                  <span> {crypto.market_cap_usd}</span>
+                  <div>
+                    <b>Percent Change 24H:</b> <span style={{color: "red"}}>{crypto.percent_change_24h}%</span>
+                  </div>
+                </div>
+            </Accordion>
+          ))}
+          </div>
         </div>
     );
-}
+};
+
 
 export default App;
